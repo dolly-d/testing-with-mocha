e66abfca931d3e1fd5fff38ceb67cd285e616fae
dolly-d <dolly.desir@gmail.com>
2021-08-16 15:30:25 -0400

    testing for data type

--- test/appTest.js
+++ test/appTest.js
@@ -2,8 +2,14 @@ const assert = require('chai').assert
 const sayHello = require('../app').sayHello
 
 describe('App', function(){
-it('app should return Your first test!', function(){
+it('sayHello should return hello', function(){
     let result = sayHello()
     assert.equal(result, 'hello')
-})
+    })
+    it('sayHello should return type string', function(){
+        let result = sayHello()
+        //setting function to variable
+        assert.typeOf(result, 'string')
+        //typeOf checks for data type
+    })
 })
